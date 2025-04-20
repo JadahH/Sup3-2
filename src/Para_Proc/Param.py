20c6de4e0e34d368aac6f9720bacbf90e4b3e8a4
JadahH <Jhol1918@students.vsu.edu>
2025-04-20 12:47:04 -0400

    declared function

--- src/Para_Proc/Param.py
+++ src/Para_Proc/Param.py
@@ -2,8 +2,20 @@ import time
 import numpy as np
 import subprocess
 
+"""
+    Print each element in the provided list with a 1-second interval between prints.
+
+    Parameters
+    ----------
+    lst : list
+        The list of items to print sequentially.
+    """
+
 def print_element(lst):
     for item in lst:
         print(item)
         time.sleep(1)
 
+
+def cube_avg():
+    pass
